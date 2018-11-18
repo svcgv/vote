ec449a8a1097093bc4c7121d136d8d0ad0dfa08c
tang <jie.tang01@hand-china.com>
2018-11-18 10:52:25 +0800

    project

--- src/main/java/com/indihx/PmProjectInfo/controller/PmProjectInfoController.java
+++ src/main/java/com/indihx/PmProjectInfo/controller/PmProjectInfoController.java
@@ -68,16 +68,14 @@ public class PmProjectInfoController {
     
     @RequestMapping(value="/queryProfitInfo",method=RequestMethod.POST)
     public @ResponseBody  Map<String,Object>  queryProfitInfo(@RequestBody Map<String, Object> param ,HttpSession session){
-//    	ProfitInfo entity = profitInfoService.queryProfitInfoByOrgId(Long.valueOf(param.get("orgId").toString()));
-//    	return R.ok().put("profitInfo", entity);
-        return null;
+    	ProfitInfo entity = profitInfoService.queryProfitInfoByOrgId(Long.valueOf(param.get("orgId").toString()));
+    	return R.ok().put("profitInfo", entity);
     }
     
     @RequestMapping(value="/queryCostInfo",method=RequestMethod.POST)
     public @ResponseBody  Map<String,Object>  queryCostInfo(@RequestBody Map<String, Object> param ,HttpSession session){
-//    	List<CostInfo> list = costInfoService.qryCostInfoListByOrgId(Long.valueOf(param.get("orgId").toString()));
-//    	return R.ok().put("page", list);
-        return null;
+    	List<CostInfo> list = costInfoService.qryCostInfoListByOrgId(Long.valueOf(param.get("orgId").toString()));
+    	return R.ok().put("page", list);
     }
     /**
      * 保存
